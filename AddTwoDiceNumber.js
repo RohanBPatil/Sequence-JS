6a867152c9cbd4a0ddf05f6fa3093bb3a6626593
RohanBPatil <rohanpatil14218@gmail.com>
2020-11-11 18:25:40 +0530

    Added : Program to get add numbers on two dice

---
+++ AddTwoDiceNumber.js
@@ -0,0 +1,5 @@
+let diceNum = Math.floor(Math.random() * 10) % 6 + 1;
+console.log("Number on dice : " + diceNum);
+let secDiceNum = Math.floor(Math.random() * 10) % 6 + 1;
+console.log("Number on second dice : " + secDiceNum);
+console.log("Sum of two numbers on dice : " + (diceNum + secDiceNum));
