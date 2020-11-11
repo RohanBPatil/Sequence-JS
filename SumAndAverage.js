8ac844a8f48b80ba440c73bc64e45bd65b871ec9
RohanBPatil <rohanpatil14218@gmail.com>
2020-11-11 18:27:36 +0530

    Added : Program to get 5 numbers and calculate sum and average

---
+++ SumAndAverage.js
@@ -0,0 +1,8 @@
+let first = Math.floor(Math.random() * 90) + 10;
+let second = Math.floor(Math.random() * 90) + 10;
+let third = Math.floor(Math.random() * 90) + 10;
+let fourth = Math.floor(Math.random() * 90) + 10;
+let fifth = Math.floor(Math.random() * 90) + 10;
+console.log("5 Number are : " + first + " " + second + " " + third + " " + fourth + " " + fifth);
+let sum = first + second + third + fourth + fifth;
+console.log("Sum : " + sum + ", Average : " + sum / 5);
